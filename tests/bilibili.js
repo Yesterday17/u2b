b0446c9f596a7aab5d9a343d18726b2bbe2a66b0
Yesterday17 <t@yesterday17.cn>
2018-07-09 02:01:10 +0800

    增加上传测试信息

--- tests/bilibili.js
+++ tests/bilibili.js
@@ -6,6 +6,9 @@ let cfg = {
   video: [`${path}/video.flv`],
   cover: `${path}/cover.png`,
 
+  type: 1,
+  from: "https://www.youtube.com/watch?v=VCyyUelzZZc",
+
   title: "Forever Stranded LS Ep1 Oh No, not again",
   description: `I feel like a bowl of petunia's right about now. 
 
@@ -13,7 +16,8 @@ let cfg = {
   tags: Array.from(new Set([...config.tags, "Minecraft", "direwolf20"])).slice(
     0,
     10
-  )
+  ),
+  dynamic: `#direwolf20# #Minecraft# #y2b#`
 };
 
 new bilibili()
