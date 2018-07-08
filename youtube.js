5d46c87ae50a1a6a7363f93ecf7b1255c4918b0f
Yesterday17 <t@yesterday17.cn>
2018-07-08 21:57:20 +0800

    开始Youtube下载

---
+++ youtube.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+
+ytdl("https://www.youtube.com/watch?v=VCyyUelzZZc", { quality: "highest" })
+  .pipe(fs.createWriteStream("./assets/video.flv"))
+  .on("progress", (chunk, download, total) => {
+    console.log(`Chunk: ${chunk}, Download: ${download}, Total: ${total}`);
+  });
