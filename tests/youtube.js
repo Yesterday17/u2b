72be433357d811223acfd92b1f8a0cc3ea3085e1
Yesterday17 <t@yesterday17.cn>
2018-07-09 01:50:53 +0800

    修复path名称

--- tests/youtube.js
+++ tests/youtube.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
-const path = "./assets/VCyyUelzZZc/video.flv";
+const path = "./assets/VCyyUelzZZc";
 
 if (fs.existsSync(path)) {
   fs.rmdirSync(path);
@@ -8,7 +8,7 @@ if (fs.existsSync(path)) {
 
 fs.mkdirSync(path);
 ytdl("https://www.youtube.com/watch?v=VCyyUelzZZc", { quality: "highest" })
-  .pipe(fs.createWriteStream(path))
+  .pipe(fs.createWriteStream(`${path}/video.flv`))
   .on("progress", (chunk, download, total) => {
     console.log(`Chunk: ${chunk}, Download: ${download}, Total: ${total}`);
   });
